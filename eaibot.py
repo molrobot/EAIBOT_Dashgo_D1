527f81433df6af65ab02dce55492d79023a1dd6e
molrobot <john19990708@gmail.com>
2020-07-27 14:39:46 +0800

    modified:   eaibot.py

--- eaibot.py
+++ eaibot.py
@@ -4,8 +4,6 @@ import time
 import math
 from queue import Queue
 
-
-
 class D1(threading.Thread):
     def __init__(self, com_port, baudrate=115200):
         threading.Thread.__init__(self)
@@ -23,13 +21,26 @@ class D1(threading.Thread):
         self.serial.timeout = 0.5
         self.serial.writeTimeout = 0.5
 
+        # 每1/30秒產生的編碼器脈衝數
+        self.ticks = 10
+        # 輪子直徑
+        self.wheel_diameter = 0.1260
+        # 左右輪間距
+        self.wheel_track = 0.3500
+        # 輪子轉一圈產生的脈衝數
+        self.encoder_resolution = 1200
+        # PWM調節頻率
+        self.PID_RATE = 30
+        self.ticks_per_meter = self.encoder_resolution / (self.wheel_diameter * math.pi)
+        self.speed = self.ticks * self.PID_RATE / self.ticks_per_meter
+
         self.input_queue = Queue()
         self.output_queue = Queue()
         self.semaphore = threading.Semaphore(2)
         self.stop_event = threading.Event()
         t1 = threading.Thread(target=self.execute, args=())
         t1.start()
-        
+
     # 旋轉
     def spin(self, speed=5, direction=True):
         print("Spinning.")
@@ -42,7 +53,7 @@ class D1(threading.Thread):
             self.input_queue.put(s)
             time.sleep(self.command_waiting_time)
         self.pause()
-    
+
     # 前進
     def forward(self, speed=5):
         print("Forwarding.")
@@ -57,7 +68,7 @@ class D1(threading.Thread):
     def backward(self, speed=5):
         print("Backwarding.")
         self.stop_event.clear()
-        while not self.stop_event.wait(self.command_waiting_time):
+        while not self.stop_event.wait(0):
             s = "z -%s -%s\r" % (speed, speed)
             self.input_queue.put(s)
         self.pause()
@@ -65,15 +76,15 @@ class D1(threading.Thread):
     # 左轉
     def left(self, speed):
         print("Turning left.")
-        
+
         self.pause()
 
     # 右轉
     def right(self, speed):
         print("Turning right.")
-        
+
         self.pause()
-        
+
     # 暫停
     def pause(self):
         self.stop_event.set()
@@ -83,21 +94,19 @@ class D1(threading.Thread):
         except:
             pass
 
-    def speed(self):
-        pass
+    def second(self, distance):
+        return distance / self.speed / 100 / 2
 
     def execute(self):
         while True:
             cammand = self.input_queue.get()
             self.semaphore.acquire()
             try:
-                self.serial.flush()
                 self.serial.write(cammand.encode())
-                time.sleep(self.command_waiting_time)
-                while self.serial.in_waiting:
-                    self.output_queue.put(self.serial.readline().decode())
+                self.output_queue.put(self.serial.read_until("\r").decode())
             except:
-                pass
+                print("execute exception")
+                self.output_queue.put("10 10 10 10")
             self.semaphore.release()
 
     # 超音波測距
@@ -125,19 +134,28 @@ def main():
                 obstacle = True
                 break
 
-        # 後退
-        # 轉向
         if obstacle:
-            t2 = threading.Thread(target=s.spin, args=(10,True))
+            # 後退
+            distance = data[3] if data[3] != 0 else 100
+            t2 = threading.Thread(target=s.backward, args=(10,))
             t2.start()
-            time.sleep(3)
+            t2.join(s.second(distance))
             s.stop_event.set()
+            
+            # 轉向
+            t3 = threading.Thread(target=s.spin, args=(10,True))
+            t3.start()
+            t3.join(3)
+            s.stop_event.set()
+
         else:
             # 前進
-            t2 = threading.Thread(target=s.forward, args=(10,))
-            t2.start()
-            time.sleep(2)
+            distance = min(data[i] for i in range(3) if data[i] > 0)
+            t4 = threading.Thread(target=s.forward, args=(10,))
+            t4.start()
+            t4.join(s.second(distance))
             s.stop_event.set()
+
         while not s.input_queue.empty():
             s.input_queue.get()
         print("next")
